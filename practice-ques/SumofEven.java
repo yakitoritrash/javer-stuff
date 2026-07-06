3ce77f8942d21c66a57798214aa15a44fd46a799
Arch me <kishudwivedi30@gmail.com>
2026-07-07 00:22:17 +0530

    Sum of Even

---
+++ practice-ques/SumofEven.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class SumofEven {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    int arr_size = s.nextInt();
+    int count = 0;
+    for (int i =0; i < arr_size; i++) {
+      int curr = s.nextInt();
+      if (curr % 2 == 0) {
+        count += curr;
+      }
+    }
+    System.out.println(count);
+    s.close();
+  }
+}
